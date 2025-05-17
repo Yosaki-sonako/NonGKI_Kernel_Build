89de82ec91ebaa4ac1603563dadbf31761eeaed9
sonako <79687386+sonako0122@users.noreply.github.com>
2025-05-17 21:11:20 +0800

    Removed the original default free disk space solution from the example YAML file. Added a new Remove useless tools solution in the example YAML file to free up space. Added a separate variable in t…

--- Patches/backport_patches.sh
+++ Patches/backport_patches.sh
@@ -1,30 +1,56 @@
 #!/bin/bash
 # Patches author: backslashxx @Github
 # Shell authon: JackA1ltman <cs2dtzq@163.com>
-# Tested kernel versions: 5.4, 4.19, 4.14, 4.9
+# Tested kernel versions: 5.4, 4.19, 4.14, 4.9, 4.4
 # 20250323
 patch_files=(
     fs/namespace.c
     fs/internal.h
     kernel/cred.c
+    kernel/trace/bpf_trace.c
+    kernel/trace/trace_kprobe.c
     include/linux/cred.h
     include/linux/uaccess.h
     mm/maccess.c
 )
 
+KERNEL_VERSION=$(head -n 3 Makefile | grep -E 'VERSION|PATCHLEVEL' | awk '{print $3}' | paste -sd '.')
+SECOND_VERSION=$(echo "$KERNEL_VERSION" | awk -F '.' '{print $2}')
+
 for i in "${patch_files[@]}"; do
+
+    if grep -q "path_umount" "$i"; then
+        echo "Warning: $i contains KernelSU"
+        continue
+    elif grep -q "get_cred_rcu" "$i"; then
+        echo "Warning: $i contains KernelSU"
+        continue
+    elif grep -q "strncpy_from_user_nofault" "$i"; then
+        echo "Warning: $i contains KernelSU"
+        continue
+    fi
 
     case $i in
 
     # fs/ changes
     ## fs/namespace.c
     fs/namespace.c)
-        sed -i '/^static bool is_mnt_ns_file/i static int can_umount(const struct path *path, int flags)\n\{\n\tstruct mount *mnt = real_mount(path->mnt);\n\tif (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))\n\t\treturn -EINVAL;\n\tif (!may_mount())\n\t\treturn -EPERM;\n\tif (path->dentry != path->mnt->mnt_root)\n\t\treturn -EINVAL;\n\tif (!check_mnt(mnt))\n\t\treturn -EINVAL;\n\tif (mnt->mnt.mnt_flags & MNT_LOCKED)\n\t\treturn -EINVAL;\n\tif (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))\n\t\treturn -EPERM;\n\treturn 0;\n}\n' fs/namespace.c
-        sed -i '/^static bool is_mnt_ns_file/i int path_umount(struct path *path, int flags)\n{\n\tstruct mount *mnt = real_mount(path->mnt);\n\tint ret;\n\tret = can_umount(path, flags);\n\tif (!ret)\n\t\tret = do_umount(mnt, flags);\n\tdput(path->dentry);\n\tmntput_no_expire(mnt);\n\treturn ret;\n}\n' fs/namespace.c
+        if grep -q "static inline bool may_mandlock(void)" fs/namespace.c; then
+            sed -i '/^static bool is_mnt_ns_file/i static int can_umount(const struct path *path, int flags)\n\{\n\tstruct mount *mnt = real_mount(path->mnt);\n\tif (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))\n\t\treturn -EINVAL;\n\tif (!may_mount())\n\t\treturn -EPERM;\n\tif (path->dentry != path->mnt->mnt_root)\n\t\treturn -EINVAL;\n\tif (!check_mnt(mnt))\n\t\treturn -EINVAL;\n\tif (mnt->mnt.mnt_flags & MNT_LOCKED)\n\t\treturn -EINVAL;\n\tif (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))\n\t\treturn -EPERM;\n\treturn 0;\n}\n' fs/namespace.c
+            sed -i '/^static bool is_mnt_ns_file/i int path_umount(struct path *path, int flags)\n{\n\tstruct mount *mnt = real_mount(path->mnt);\n\tint ret;\n\tret = can_umount(path, flags);\n\tif (!ret)\n\t\tret = do_umount(mnt, flags);\n\tdput(path->dentry);\n\tmntput_no_expire(mnt);\n\treturn ret;\n}\n' fs/namespace.c
+        else
+            sed -i '/SYSCALL_DEFINE2(umount, char __user \*, name, int, flags)/i\#ifdef CONFIG_KSU\nstatic int can_umount(const struct path *path, int flags)\n{\n\tstruct mount *mnt = real_mount(path->mnt);\n\n\tif (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))\n\t\treturn -EINVAL;\n\tif (!may_mount())\n\t\treturn -EPERM;\n\tif (path->dentry != path->mnt->mnt_root)\n\t\treturn -EINVAL;\n\tif (!check_mnt(mnt))\n\t\treturn -EINVAL;\n\tif (mnt->mnt.mnt_flags & MNT_LOCKED) /* Check optimistically */\n\t\treturn -EINVAL;\n\tif (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))\n\t\treturn -EPERM;\n\treturn 0;\n}\n\nint path_umount(struct path *path, int flags)\n{\n\tstruct mount *mnt = real_mount(path->mnt);\n\tint ret;\n\n\tret = can_umount(path, flags);\n\tif (!ret)\n\t\tret = do_umount(mnt, flags);\n\n\t/* we mustn'\''t call path_put() as that would clear mnt_expiry_mark */\n\tdput(path->dentry);\n\tmntput_no_expire(mnt);\n\treturn ret;\n}\n#endif\n' fs/namespace.c
+        fi
         ;;
     ## fs/internal.h
     fs/internal.h)
-        sed -i '/^extern void __init mnt_init/a int path_umount(struct path *path, int flags);' fs/internal.h
+        if [ "$SECOND_VERSION" -le 9 ]; then
+            if grep -q "extern void __mnt_drop_write(struct vfsmount \*)" fs/internal.h; then
+                sed -i '/extern void __mnt_drop_write_file(struct file \*);/a int path_umount(struct path \*path, int flags);' fs/internal.h
+            else
+                sed -i '/^extern void __init mnt_init/a int path_umount(struct path *path, int flags);' fs/internal.h
+            fi
+        fi
         ;;
 
     # kernel/ changes
@@ -34,6 +60,19 @@ for i in "${patch_files[@]}"; do
             sed -i "s/!atomic_long_inc_not_zero(&((struct cred \*)cred)->usage)/!get_cred_rcu(cred)/g" kernel/cred.c
         else
             sed -i "s/!atomic_inc_not_zero(&((struct cred \*)cred)->usage)/!get_cred_rcu(cred)/g" kernel/cred.c
+        fi
+        ;;
+    ## kernel/trace
+    ### kernel/trace/bpf_trace.c
+    kernel/trace/bpf_trace.c)
+        if [ "$KERNEL_VERSION" == "5.4" ]; then
+            sed -i 's/\bstrncpy_from_unsafe_user\b/strncpy_from_user_nofault/g' kernel/trace/bpf_trace.c
+        fi
+        ;;
+    ### kernel/trace/trace_kprobe.c
+    kernel/trace/trace_kprobe.c)
+        if [ "$KERNEL_VERSION" == "5.4" ]; then
+            sed -i 's/\bstrncpy_from_unsafe_user\b/strncpy_from_user_nofault/g' kernel/trace/trace_kprobe.c
         fi
         ;;
 
